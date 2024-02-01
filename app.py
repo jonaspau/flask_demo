ec73bdc7620068faf63a706482c8cc6353aa529e
JonasP <jonaspau@outlook.com>
2024-02-01 14:38:39 +0100

    Completed project

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/cakes')
+def cakes():
+    return render_template('cakes.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
